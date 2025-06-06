544b6caabe0655c897d34b038f0620e6d8c91a97
Estefania <aida.estefania.sosa@gmail.com>
2025-06-06 16:40:29 +0200

    final-version w/o README

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 from utils.openspace import Openspace
-from utils.table import Table, Seat
+from utils.table import Table
 from utils.file_utils import load_names
 
 
 if __name__ == "__main__":
 
-    colleagues_names = load_names("./data/colleagues.csv")
+    """
+    Main function which executes the program
+    and establishes initial conditions
+    """
 
+    colleagues_names = load_names("./data/colleagues.csv")
     table_capacity = 4
     number_of_tables = 6
+
+    while len(colleagues_names) > table_capacity * number_of_tables:
+        number_of_tables += 1
 
     tables = [Table(table_capacity) for i in range(0, number_of_tables)]
 
     openspace = Openspace(tables, number_of_tables)
     openspace.organize(colleagues_names)
-
-    print(colleagues_names)
-
     openspace.display()
     # openspace.store("seats_assigned.csv")
